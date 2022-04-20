ff38c0f9501b3b64178dec401c68d07515155938
martinamanno <93279049+martinamanno@users.noreply.github.com>
2022-04-20 15:55:45 +0200

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -367,3 +367,180 @@ dfp = dfp[:414]
 
 
 
+#LSTM
+
+#https://towardsdatascience.com/predicting-sales-611cb5a252de
+from datetime import datetime, timedelta,date
+import pandas as pd
+%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+from __future__ import division#import Keras
+import warnings
+warnings.filterwarnings("ignore")
+from chart_studio import plotly
+import plotly.offline as pyoff
+import plotly.graph_objs as go
+
+#read the data in csv
+df= pd.read_csv('/Users/martinamanno/Desktop/LUISS/CORSI da seguire/DATA SCIENCE IN ACTION/DELOITTE/progetto 2/dataset.csv')
+
+#convert date field from string to datetime
+df['day'] = pd.to_datetime(df['day'])
+
+#show first 10 rows
+df.head(10)
+
+df['online_purchases']=df['convrate']* df['visits']
+df['sales']= df['online_purchases']* df['avspend']
+df.head(10)
+
+#represent month in date field as its first day
+#df['day'] = df['day'].dt.year.astype('str') + '-' + df['day'].dt.month.astype('str') + '-01'
+#df['day'] = pd.to_datetime(df['day'])
+
+#groupby date and sum the sales
+#df = df.groupby('day').sales.sum().reset_index()
+
+#plot monthly sales
+plot_data = [
+    go.Scatter(
+        x=df['day'],
+        y=df['sales'],
+    )
+]
+plot_layout = go.Layout(
+        title='Montly Sales'
+    )
+fig = go.Figure(data=plot_data, layout=plot_layout)
+pyoff.iplot(fig)
+
+#create a new dataframe to model the difference
+df_diff = df.copy()
+#add previous sales to the next row
+df_diff['prev_sales'] = df_diff['sales'].shift(1)
+#drop the null values and calculate the difference
+df_diff = df_diff.dropna()
+df_diff['diff'] = (df_diff['sales'] - df_diff['prev_sales'])
+df_diff.head(10)
+
+#plot sales diff
+plot_data = [
+    go.Scatter(
+        x=df_diff['day'],
+        y=df_diff['diff'],
+    )
+]
+plot_layout = go.Layout(
+        title='Montly Sales Diff'
+    )
+fig = go.Figure(data=plot_data, layout=plot_layout)
+pyoff.iplot(fig)
+
+#create dataframe for transformation from time series to supervised
+df_supervised = df_diff.drop(['prev_sales'],axis=1)
+#adding lags
+for inc in range(1,13):
+    field_name = 'lag_' + str(inc)
+    df_supervised[field_name] = df_supervised['diff'].shift(inc)
+    #drop null values
+df_supervised = df_supervised.dropna().reset_index(drop=True)
+df_supervised
+
+# Import statsmodels.formula.api
+import statsmodels.formula.api as smf
+# Define the regression formula
+#model = smf.ols(formula='diff ~ lag_1 + lag_2 +lag_3 + lag_4 + lag_5 + lag_6 + lag_7 + lag_8 + lag_9 + lag_10 + lag_11 + lag_12', data=df_supervised)
+model = smf.ols(formula='diff ~  lag_1  + lag_2 +lag_3 + lag_4 + lag_5 + lag_6 + lag_7 + lag_8 + lag_9 + lag_10 + lag_11 + lag_12', data=df_supervised)
+# Fit the regression
+model_fit = model.fit()
+# Extract the adjusted r-squared
+regression_adj_rsq = model_fit.rsquared_adj
+print(regression_adj_rsq)
+
+#import MinMaxScaler and create a new dataframe for LSTM model
+from sklearn.preprocessing import MinMaxScaler
+df_model = df_supervised.drop(['sales','day'],axis=1)
+#split train and test set
+train_set, test_set = df_model[0:-6].values, df_model[-6:].values
+df_model
+
+#apply Min Max Scaler
+scaler = MinMaxScaler(feature_range=(-1, 1))
+scaler = scaler.fit(train_set)
+
+# reshape training set
+train_set = train_set.reshape(train_set.shape[0], train_set.shape[1])
+train_set_scaled = scaler.transform(train_set)
+# reshape test set
+test_set = test_set.reshape(test_set.shape[0], test_set.shape[1])
+test_set_scaled = scaler.transform(test_set)
+
+X_train, y_train = train_set_scaled[:, 1:], train_set_scaled[:, 0:1]
+X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])
+X_test, y_test = test_set_scaled[:, 1:], test_set_scaled[:, 0:1]
+X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])
+
+#import Keras
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.optimizers import Adam 
+from keras.callbacks import EarlyStopping
+from keras.utils import np_utils
+from keras.layers import LSTM
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+
+model = Sequential()
+model.add(LSTM(4, batch_input_shape=(1, X_train.shape[1], X_train.shape[2]), stateful=True))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(X_train, y_train, nb_epoch=100, batch_size=1, verbose=1, shuffle=False)
+
+y_pred = model.predict(X_test,batch_size=1)
+#reshape y_pred
+y_pred = y_pred.reshape(y_pred.shape[0], 1, y_pred.shape[1])
+#rebuild test set for inverse transform
+pred_test_set = []
+for index in range(0,len(y_pred)):
+    print np.concatenate([y_pred[index],X_test[index]],axis=1)
+    pred_test_set.append(np.concatenate([y_pred[index],X_test[index]],axis=1))
+#reshape pred_test_set
+pred_test_set = np.array(pred_test_set)
+pred_test_set = pred_test_set.reshape(pred_test_set.shape[0], pred_test_set.shape[2])
+#inverse transform
+pred_test_set_inverted = scaler.inverse_transform(pred_test_set)
+
+#create dataframe that shows the predicted sales
+result_list = []
+sales_dates = list(df_sales[-7:].date)
+act_sales = list(df_sales[-7:].sales)
+for index in range(0,len(pred_test_set_inverted)):
+    result_dict = {}
+    result_dict['pred_value'] = int(pred_test_set_inverted[index][0] + act_sales[index])
+    result_dict['date'] = sales_dates[index+1]
+    result_list.append(result_dict)
+df_result = pd.DataFrame(result_list)
+#for multistep prediction, replace act_sales with the predicted sales
+
+#merge with actual sales dataframe
+df_sales_pred = pd.merge(df_sales,df_result,on='date',how='left')
+#plot actual and predicted
+plot_data = [
+    go.Scatter(
+        x=df_sales_pred['date'],
+        y=df_sales_pred['sales'],
+        name='actual'
+    ),
+        go.Scatter(
+        x=df_sales_pred['date'],
+        y=df_sales_pred['pred_value'],
+        name='predicted'
+    )
+    
+]
+plot_layout = go.Layout(
+        title='Sales Prediction'
+    )
+fig = go.Figure(data=plot_data, layout=plot_layout)
+pyoff.iplot(fig)
